5e701902cabb1de052f68d17f41c4fbfc765f647
Xavier Góngora <ixbalanque@protonmail.ch>
2024-04-04 16:05:54 -0600

    spec

--- src/Sound/RTG/Ritmo/PatronRegular.hs
+++ src/Sound/RTG/Ritmo/PatronRegular.hs
@@ -3,11 +3,13 @@ module Sound.RTG.Ritmo.PatronRegular where
 -- Ritmos euclideanos utilizando un redondeo modular
 -- Esta aproximación es una adaptación del trabajo final con Mauricio Rodriguez
 
-{-@ patronRegular :: Nat -> {n : Nat | n /= 0} -> [Nat] @-}
-patronRegular :: Int -> Int -> [Int]
+{-@ assume fromInteger :: Num a => {x : Integer | x /= 0} -> {y : a | y /= 0} @-}
+
+{-@ patronRegular :: { pulses : Integer | pulses > 0} -> { onsets : Integer | onsets >= pulses} -> { xs : [Integer] | len xs > 0 }@-}
+patronRegular :: Integer -> Integer -> [Integer]
 patronRegular pulses events =
-  let p' = fromIntegral pulses :: Rational
-      e' = fromIntegral events :: Rational
+  let p' = fromInteger pulses :: Rational
+      e' = fromInteger events :: Rational
       step = e' / p'
       stepList = map round [0, step ..]
    in takeWhile (< events) stepList
